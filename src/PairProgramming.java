9774bd94ad18d394de33f707b8095ea9b92a11b8
kaitlynchan <ununoctium829@gmail.com>
2016-09-29 08:53:15 -0700

    GHIIIIIOIPS"JDLKFJ"LKS

--- src/PairProgramming.java
+++ src/PairProgramming.java
@@ -1,4 +1,32 @@
-
+import java.util.*;
 public class PairProgramming {
+	public static void main (String[] args){
+		Scanner userInput = new Scanner (System.in);
+		int evenMax = 0;
+		int min = 0;
+		int max = 0;
+		int evenTotal = 0;
+		System.out.println("How many loops do you want?");
+		int loops = userInput.nextInt();
+		for (int number = loops; number <= loops; number++){
+			System.out.println("enter in next number");
+			int nextNumber = userInput.nextInt();
+			if (nextNumber > max){
+				max = nextNumber;
+			}
+			else{
+				min = nextNumber;
+			}
+			if (nextNumber % 2 == 0){
+				evenTotal += nextNumber;
+				if(nextNumber > evenMax){
+					evenMax = nextNumber;
+				}
+			}
+			
+		}
+		System.out.println(evenTotal);
+	}
+	
 
 }
